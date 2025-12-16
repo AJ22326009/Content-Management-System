8abd5a8f2fd3a772159714ab26f9b1704cc05a7d
aj22326009 <aj22326009@utg.edu.gm>
2025-12-16 16:41:01 +0000

    routes created for articles

---
+++ backend/routes/article.routes.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const router = express.Router();
+const {
+    getAllArticles, 
+    deleteArticle, 
+    viewArticle, 
+    createArticle, 
+    editArticle, 
+    publishArticle
+} = require('../controllers/article.controller');
+
+const authorize = require('../middleware/authorize');
+
+// Create a new article
+router.post('/',
+    authorize('create_article'),
+    createArticle
+)
+
+//get all with viewer rules inside controller
+router.get('/',
+    authorize('view_article'),
+    getAllArticles
+);
+
+//get one article
+router.get('/:id',
+    authorize('view_article'),
+    viewArticle
+);
+
+//edit an article
+router.put('/:id',
+    authorize('edit_article'),
+    editArticle
+);
+
+//delete an article
+router.delete('/:id',
+    authorize('delete_article'),
+    deleteArticle
+);
+
+//publish an article
+router.put('/:id/publish',
+    authorize('publish_article'),
+    publishArticle
+);
+
+
+module.exports = router;
