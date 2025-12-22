94a9034b0ee14404295c46766b534dc038650a2b
aj22326009 <aj22326009@utg.edu.gm>
2025-12-22 20:25:15 +0000

    model redesigned

--- frontend/src/app/models/article.model.ts
+++ frontend/src/app/models/article.model.ts
@@ -4,4 +4,5 @@ export interface Article{
     body: string;
     isPublished?: boolean | false;
     imageUrl?: string | null;
+    author?: {fullname: string, email: string, _id: string} | null;
 }
