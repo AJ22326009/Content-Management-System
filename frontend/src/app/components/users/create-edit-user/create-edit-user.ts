f5b2ab5665a30dac1c2b1fa0301ac84267855b9a
aj22326009 <aj22326009@utg.edu.gm>
2025-12-23 12:40:47 +0000

    users components (users and create-edit-user) created

---
+++ frontend/src/app/components/users/create-edit-user/create-edit-user.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-edit-user',
+  imports: [],
+  templateUrl: './create-edit-user.html',
+  styleUrl: './create-edit-user.css',
+})
+export class CreateEditUser {
+
+}
