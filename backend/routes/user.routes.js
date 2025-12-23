e4c4227e043000ba9c2ff6faa4b71473952e62a1
aj22326009 <aj22326009@utg.edu.gm>
2025-12-23 11:37:57 +0000

    routes for update, delete, edit, getById and  getAll users added

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,8 +1,20 @@
 const express=require('express');
 const router=express.Router();
-const {registerUser}=require('../controllers/user.controller');
+const {registerUser, getAllUsers, getUserById, deleteUser, updateUser}=require('../controllers/user.controller');
 
 //user registration
 router.post('/register',registerUser);
+
+//get all users
+router.get('/',getAllUsers);
+
+//get user by id
+router.get('/:id',getUserById);
+
+//delete user
+router.delete('/delete/:id',deleteUser);
+
+//update user
+router.put('/update/:id',updateUser);
 
 module.exports=router;
