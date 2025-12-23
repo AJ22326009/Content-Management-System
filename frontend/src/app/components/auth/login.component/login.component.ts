7e57e51145a0398e4cf2b1dc9ddd81b2c443c232
aj22326009 <aj22326009@utg.edu.gm>
2025-12-23 09:26:28 +0000

    login form changed to reactive and validation added

--- frontend/src/app/components/auth/login.component/login.component.ts
+++ frontend/src/app/components/auth/login.component/login.component.ts
@@ -1,22 +1,28 @@
 import { Component} from '@angular/core';
 import { AuthService } from '../../../services/auth';
 import { Router } from '@angular/router';
-import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule, Validators, FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [FormsModule],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
 export class LoginComponent{
   email: string = '';
   password: string = '';
+  loginForm: FormGroup;
  
-constructor(private authService: AuthService, private router: Router) {}
+constructor(private authService: AuthService, private router: Router, private fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required]
+    });
+}
 
   login() {
-    this.authService.login({email: this.email, password: this.password}).subscribe({
+    this.authService.login({email: this.loginForm.value.email, password: this.loginForm.value.password}).subscribe({
       next: (res) => {
         // Successful login handled in AuthService
       },
