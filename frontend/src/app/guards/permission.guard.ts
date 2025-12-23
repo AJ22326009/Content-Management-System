a46df74c836e3d29c62b6949f9212754da1e9d79
aj22326009 <aj22326009@utg.edu.gm>
2025-12-23 12:43:23 +0000

    path to authservice changed

--- frontend/src/app/guards/permission.guard.ts
+++ frontend/src/app/guards/permission.guard.ts
@@ -1,6 +1,6 @@
 import { inject } from '@angular/core';
 import { CanActivateFn, Router, ActivatedRouteSnapshot } from '@angular/router';
-import { AuthService } from '../services/auth';
+import { AuthService } from '../services/auth/auth';
 
 export const permissionGuard: CanActivateFn = (route: ActivatedRouteSnapshot) => {
   const authService = inject(AuthService);
