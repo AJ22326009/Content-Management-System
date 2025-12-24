5f5374635fd62ede709925e963ded1a0fefc1744
aj22326009 <aj22326009@utg.edu.gm>
2025-12-24 15:37:24 +0000

    invalid or expired token leads to login page

--- frontend/src/app/interceptor/auth.interceptor.ts
+++ frontend/src/app/interceptor/auth.interceptor.ts
@@ -1,6 +1,12 @@
-import {HttpInterceptorFn} from '@angular/common/http';
+import {HttpErrorResponse, HttpInterceptorFn} from '@angular/common/http';
+import {inject} from '@angular/core';
+import { Router } from '@angular/router';
+import { catchError, throwError } from 'rxjs';
+import { AuthService } from '../services/auth/auth';
 
 export const AuthInterceptor: HttpInterceptorFn = (req, next) => {
+    const router = inject(Router);
+    const authService = inject(AuthService);
     const accessToken = localStorage.getItem('accessToken');
 
     if (accessToken) {
@@ -10,5 +16,20 @@ export const AuthInterceptor: HttpInterceptorFn = (req, next) => {
             }
         });
     }
-    return next(req);
+    return next(req).pipe(
+        catchError((error: HttpErrorResponse) => {
+            // case 1: token expired / invalid
+            if (error.status === 401) {
+                authService.logout();
+                router.navigate(['/login']);
+            }
+
+            // case 2: No internet / server down
+            if(error.status === 0){
+                console.error('Network Error: server unreachable.');
+            }
+
+            return throwError(() => error);
+        })
+    )
 };
