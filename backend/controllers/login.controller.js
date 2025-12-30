316f75ffd627708a4cf5f763ce27264a90ebca22
aj22326009 <aj22326009@utg.edu.gm>
2025-12-30 13:39:22 +0000

    restored to normal

--- backend/controllers/login.controller.js
+++ backend/controllers/login.controller.js
@@ -13,13 +13,13 @@ const loginUser = async(req,res)=>{
     });
 
     if(!user){
-        return  res.status(400).json({message:'invalid email'});
+        return  res.status(401).json({message:'invalid email'});
     };
 
     const isMatch=await bcrypt.compare(password,user.password);
 
     if(!isMatch){
-        return res.status(400).json({message:'invalid password'});
+        return res.status(401).json({message:'invalid password'});
     }
 
     const accessToken=jwt.sign(
